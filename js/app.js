2887777b993910142a631e43610f7ccf87edfc12
David Quinn <david@courbanize.com>
2013-11-27 17:20:40 -0500

    added in google api js + correct cartodb link

--- js/app.js
+++ js/app.js
@@ -3,17 +3,17 @@ var map;
 function main() {
 
   var options = {
-    center: [-42.27730877423707, 172.63916015625],
-    zoom: 6, 
+    center: [22.5333, 114.1333],
+    zoom: 10, 
     zoomControl: false,  // dont add the zoom overlay (it is added by default)
     loaderControl: false //dont show tiles loader
   };
 
-  cartodb.createVis('map', 'http://ryancook.cartodb.com/viz/9c358376-55f9-11e3-aeeb-078aae0fd0ec/embed_map', options)
+  cartodb.createVis('map', 'http://kmytty.cartodb.com/api/v2/viz/f0e12a5e-57ae-11e3-a874-fb377fdb1b81/viz.json', options)
     .done(function(vis, layers) {
       // there are two layers, base layer and points layer
       var sublayer = layers[1].getSubLayer(0);
-      sublayer.set({ 'interactivity': ['cartodb_id', 'name_to_display', 'description'] });
+      sublayer.set({ 'interactivity': ['cartodb_id', 'govt', 'url'] });
 
       // Set the custom infowindow template defined on the html
       sublayer.infowindow.set('template', $('#infowindow_template').html());
@@ -21,12 +21,12 @@ function main() {
       // add the tooltip show when hover on the point
       vis.addOverlay({
         type: 'tooltip',
-        template: '<p>{{name_to_display}}</p>'
+        template: '<p>{{govt}}</p>'
       });
 
       vis.addOverlay({
         type: 'infobox',
-        template: '<h3>{{name_to_display}}</h3><p>{{description}}</p>',
+        template: '<h3>{{govt}}</h3><p>{{url}}</p>',
         width: 400,
         position: 'bottom|right'
       });
